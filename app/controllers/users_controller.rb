d2afd85b2c81f604a132b42ba716356291b3ccc7
taisaoko <ttd3calvin@gmail.com>
2019-09-03 16:25:47 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,4 +26,23 @@ class UsersController < ApplicationController
       redirect to '/tweets'
     end
   end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/tweets"
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 end
